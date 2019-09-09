469f5c2beb6af5fc0ece80caff238e6b96643581
Deivi Robles <drozam24@gmail.com>
2019-09-09 18:16:43 -0500

    WIP: Playing with Router oprions

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import NavBar from './components/NavBar';
 import Tasks from './pages/Tasks';
 import Projects from './pages/Projects';
 import Users from './pages/Users';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <Fragment>
       <NavBar />
       <Switch>
-        <Route path="/tasks" component={Tasks} />
-        <Route path="/projects" component={Projects} />
+        <Route path="/tasks" render={(props) => <Tasks {...props} user="Gustavo" />} />
+        <Route path="/projects/:id" component={Projects} />
         <Route path="/users" component={Users} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
         <Redirect to="/tasks" />
       </Switch>
     </Fragment>
