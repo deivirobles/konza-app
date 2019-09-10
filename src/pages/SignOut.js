2a903514e72ddf21798712768b47496d9bf3d3e5
Deivi Robles <drozam24@gmail.com>
2019-09-09 19:23:38 -0500

    Add AuthNavBar and SignOut

---
+++ src/pages/SignOut.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { removeToken } from "../services/auth";
+import { Redirect } from "react-router-dom";
+
+export default function SignOut({ clearUser }) {
+  removeToken();
+  clearUser();
+
+  return (
+    <Redirect to="/" />
+  )
+}
